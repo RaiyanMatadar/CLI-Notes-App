435c04e91e4adf6384f55a65395737c276e7e662
RaiyanMatadar <raiyanmatadar001@gmail.com>
2026-08-01 18:45:32 +0530

    change the whole notes-cli.js code with other approach -BUG (data is getting added in the notes without overriding the data but the problem is the data is getting nested inside the notes.json which is an problem)

--- notes-cli.js
+++ notes-cli.js
@@ -1,55 +1,69 @@
 const fs = require("fs");
 const path = require("path");
+const configPath = path.join(__dirname, 'notes.json');
 
 // for adding note commands
 const command = process.argv[2]; // e.g "add"
-const noteText = process.argv[3]; // e.g "Buy groceries"
 
-// if the command is "node notes-cli.js" then the below will run
+let id = 0;
+const noteText = process.argv[3]; // e.g "Buy groceries"
+const createdAt = new Date();
+
+// this notes obj will hold data of single note everytime i run 
+// - node notes-cli.js add "show"
+const notes = {
+  id: id,
+  noteText: noteText,
+  createdAt: createdAt
+}
+
+
+// if the command is "node notes-cli.js add" then the below will run
 if (command === "add") {
+
   // validation condition if the noteText isnt provided then it will trow error
   if (noteText == undefined) {
     console.log("please enter note");
     return;
   }
 
-  let data = [];
+  try {
+    // if file `notes.json` exist then add data else make the `notes.json` file  
+    if (fs.existsSync(configPath)) {
 
-  // check if the notStrictEqual.json file exist
-  if (fs.promises.access("notes.json")) {
-    // then we are getting that data
-    fs.readFile("notes.json", async (error, existingData) => {
-      
-      if (error) return;
-      await data.push(existingData.toString())
+      // read the whole file and add that data into `data` param
+      fs.readFile(configPath, (error, data) => {
+        if (error) return console.log(error);
 
-      console.log("inside async");
+        // everytime i make new note, it will pushed to existingNotes array
+        let existingNotes = []
+        existingNotes.push(notes)
+        
       
-    });
+        // if `notes.json` has data then we will send notes.json 
+        // data into existingNotes variable as an object 
+        if (data.length > 0) {
+          existingNotes.push(JSON.parse(data))
+          console.log(existingNotes);
+        }
+        
+
+        fs.writeFile(configPath, JSON.stringify(existingNotes, null, 2), (error) => {
+          if (error) return
+
+          
+        })
+      })
+
+
+    } else {
+      fs.writeFile(configPath, JSON.stringify(notes), (error) => {
+        if (error) return console.log(error);
+      })
+    }
+
+  } catch (error) {
+    console.log("this is catch error ", error);
   }
 
-  console.log("outside async");
-
-  // here the noteText in the param will have the data which is passed by user
-  // and below we are using that data to add it in the notes.json file
-  let id = 0;
-  const createdAt = new Date().toISOString();
-
-  const note = {
-    id: id,
-    createdAt: createdAt,
-    noteText: noteText,
-  };
-
-  data.push(note);
-
-  // adding data.text into notes.json file
-  fs.writeFile("notes.json", JSON.stringify(data), (error) => {
-    if (error) return;
-    
-    console.log("note added succesfully");
-  });
-
-  // increment id by 1 for adding unique id everytime user enter note
-  id++;
 }
